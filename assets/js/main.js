377d638a557104e30e2835746833418d534153a6
Hugo De La Cruz <jhugodc@gmail.com>
2023-11-09 15:04:28 -0700

    Add button to show talk description

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,6 +212,7 @@ function getNameIds() {
 }
 
 function getSessionHtml(session, name_ids) {
+  var session_id = session.id;
   var session_name = session.name;
   var session_description = session.description;
   var session_start_time = session.start;
@@ -229,7 +230,7 @@ function getSessionHtml(session, name_ids) {
   var session_start_time = getIstanbulTime(session_start);
   var session_end_time = getIstanbulTime(session_end);
   var times = session_start_time + " - " + session_end_time;
-  var session_html = "<section><h2>" + session_name + "</h2><h3>" + times + "</h3>"
+  var session_html = "<section id=\"session_" + session_id + "\"><h2>" + session_name + "</h2><h3>" + times + "</h3>"
 
   for (var j = 0; j < session_speakers.length; j++) {
     var session_speaker = session_speakers[j];
@@ -249,7 +250,10 @@ function getSessionHtml(session, name_ids) {
     session_html += "<img class=\"circular-square\" src=\"images/speakers/"+ session_speaker_id +".jpg\" alt=\"" + session_speaker_name + "\" />";
   }
 
-  session_html +="<h4>" + session_speaker_names_string + "</h4><p>" + session_description + "</p></section>";
+  session_html += "<h4>" + session_speaker_names_string + "</h4>";
+  session_html += "<ul class=\"actions\"><li><a id=\"show_desc_" + session_id + "\" onclick=\"showDescription('" + session_id + "')\" href=\"#session_" + session_id + "\" class=\"button scrolly\">Show Talk Description</a></li></ul>";
+  session_html += "<p id=\"session_desc" + session_id + "\" class=\"hidden\">" + session_description + "</p>";
+  session_html += "<ul class=\"actions\"><li><a id=\"hide_desc_" + session_id + "\" onclick=\"hideDescription('" + session_id + "')\" href=\"#session_" + session_id + "\" class=\"button scrolly hidden\">Hide Description</a></li></ul></section>";
   return session_html;
 }
 
@@ -265,17 +269,32 @@ function loadSessions() {
       session_html = getSessionHtml(session, name_ids);
       $("#sessions-main").append(session_html);
     }
-  });
+  }).then(function () {
+    $.ajax({
+      url: "https://app.streameth.org/api/organizations/devconnect/events/evm_summit/sessions?stage=emirgan_2"
+    }).then(function(sessions) { // Breakout Stage
+      $("#sessions-breakout").append("<h2>Emirgan 2</h2>");
+      for (var i = 0; i < sessions.length; i++) {
+        var session = sessions[i];
+        session_html = getSessionHtml(session, name_ids);
+        $("#sessions-breakout").append(session_html);
+      }
+    }).then(function() {
+      // Update scrolly.
+      $('.scrolly').scrolly({
+        speed: 1000,
+        offset: function() {
 
-  $.ajax({
-    url: "https://app.streameth.org/api/organizations/devconnect/events/evm_summit/sessions?stage=emirgan_2"
-  }).then(function(sessions) { // Breakout Stage
-    $("#sessions-breakout").append("<h2>Emirgan 2</h2>");
-    for (var i = 0; i < sessions.length; i++) {
-      var session = sessions[i];
-      session_html = getSessionHtml(session, name_ids);
-      $("#sessions-breakout").append(session_html);
-    }
+          // If <=large, >small, and sidebar is present, use its height as the offset.
+            if (breakpoints.active('<=large')
+            &&	!breakpoints.active('<=small')
+            &&	$sidebar.length > 0)
+              return $sidebar.height();
+
+          return 0;
+        }
+      });
+    });
   });
 }
 
@@ -322,4 +341,24 @@ function hideBio(bio_id) {
 
   show_bio_link.style.display = "block";
   bio.style.display = "none";
+}
+
+function showDescription(session_id) {
+  var show_desc_link = document.getElementById("show_desc_" + session_id);
+  var hide_desc_link = document.getElementById("hide_desc_" + session_id);
+  var desc = document.getElementById("session_desc" + session_id);
+
+  show_desc_link.classList.add("hidden");
+  hide_desc_link.classList.remove("hidden");
+  desc.classList.remove("hidden");
+}
+
+function hideDescription(session_id) {
+  var show_desc_link = document.getElementById("show_desc_" + session_id);
+  var hide_desc_link = document.getElementById("hide_desc_" + session_id);
+  var desc = document.getElementById("session_desc" + session_id);
+
+  show_desc_link.classList.remove("hidden");
+  hide_desc_link.classList.add("hidden");
+  desc.classList.add("hidden");
 }
